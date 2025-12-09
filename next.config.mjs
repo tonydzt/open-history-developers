ef92e14aef737a17db01f5d520d26502afcb277c
douzhitong <douzhitong@pwrd.com>
2025-12-09 17:27:41 +0800

    增加根目录重定向

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,15 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/',  // 当访问根路径时
+        destination: '/en',  // 重定向到默认的语言路径（例如：'/en'）
+        permanent: true,  // 永久重定向
+      },
+    ];
+  },
 };
 
 export default withMDX(config);
